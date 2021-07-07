4a4d2f3cbeab39d8230c3a04bd841e7b725a9015
Mai Thanh <maiduyhungthanh@gmail.com>
2021-07-07 14:47:22 +0700

    0k

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,5 +1,38 @@
+
+import java.util.Scanner;
+
 public class Ex1 {
+
     public static void main(String[] args) {
-        System.out.println(" CHƯA LÀM ĐƯỢC");
+	
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Số chuỗi tuỳ chọn");
+        int n = Integer.valueOf(sc.nextLine());
+        System.out.println("Nhập phần tử chuỗi: ");
+        String [] A= new String[n];
+        for (int i = 0; i < n; i++) {
+            System.out.println("A[" + i + "] = "  );
+            A[i] = sc.nextLine();
+        }
+        System.out.println("Phần tử chuỗi cần kiểm tra: ");
+         String isCheck = sc.nextLine();
+        String b = "";
+        String Java = "Java";
+        int jv = 0;
+        for (int i = 0; i < n; i++) {
+            if (A[i].equalsIgnoreCase(Java)) {
+                jv++;
+            }
+            if (isCheck.equals(A[i])) {
+                b += i + " ";
+            }
+        }
+        System.out.println(" - Số lần ''Java'' xuất hiện trong chuỗi là " + jv);
+        if (b.length() > 0) {
+            System.out.println(" - Phân tử [" + isCheck + "] xuất hiện ở vị trí " + b);
+        } else {
+            System.out.println(" - Phân tử [" + isCheck + "] không xuất hiện trong chuỗi ");
+        }
+        sc.close();
     }
 }
